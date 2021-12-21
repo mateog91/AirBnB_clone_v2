37f945e09d9e9e0853fa4fe1ff33c8b107691a01
Mateo <mateog91@gmail.com>
2021-12-21 15:14:25 -0500

    UPD: CHANGES FOR TASK 6

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class to manage db storage for hbnb clone"""
+from sqlalchemy import (create_engine)
+from sqlalchemy.sql.schema import MetaData
+from models.base_model import Base
+import os
 
 
 class DBStorage:
@@ -9,5 +13,11 @@ class DBStorage:
 
     def __init__(self):
         """initialization of db"""
-        __self.engine
+        HBNB_MYSQL_USER = os.getenv('HBNB_MYSQL_USER')
+        self.__engine = create_engine(
+            f"mysql+mysqldb://{os.getenv('HBNB_MYSQL_USER')}:{os.getenv('HBNB_MYSQL_PWD')}@{os.getenv('HBNB_MYSQL_HOST')}/{os.getenv('HBNB_MYSQL_DB')}", pool_pre_ping=True)
 
+        if os.getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
--- models/state.py
+++ models/state.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.sql.expression import delete
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+import os
+from models import storage
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state')
+
+    HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
+    if HBNB_TYPE_STORAGE == 'db':
+        cities = relationship('City', backref='state', cascade='delete')
+
+    else:
+        @property
+        def cities(self):
+            cities = storage.all(City)
+            return [value for value in cities.values() if self.id == value.state_id]
