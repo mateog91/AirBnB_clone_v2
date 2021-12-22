a41b89ac85e30649764abd49ad706da2c17c9ddc
Mateo <mateog91@gmail.com>
2021-12-22 12:18:51 -0500

    UPD: CHANGES IN CITY AND PLACES, CURRENTLY PASSES TEST OF TASKS 7 AND 8

--- models/place.py
+++ models/place.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(Strign(60), ForeignKey('users.id', nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
@@ -16,4 +17,4 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
+    # amenity_ids = []
