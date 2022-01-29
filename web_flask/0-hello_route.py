085013aa8819186e2c6f4460b9ce34092cd6036b
Mateo <mateog91@gmail.com>
2022-01-29 16:55:14 -0500

    FIX: CORRECT CODE

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,11 +6,15 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/hbnb')
-def hello_hbnb2():
-    """ displays HBNB for this route /hbnb”
+@app.route('/')
+def hello_hbnb():
+    """Write a script that starts a Flask web application:
+    Your web application must be listening on 0.0.0.0, port 5000
+    Routes:
+        /: display “Hello HBNB!”
+    You must use the option strict_slashes=False in your route definition
     """
-    return 'HBNB'
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
