19887c3caa4e54e95db01c8a06328920deecb1e7
Mateo <mateog91@gmail.com>
2022-01-31 00:49:34 -0500

    changes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-Module for task 7
+starts with flask
 """
-
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -13,18 +12,16 @@ app.url_map.strict_slashes = False
 
 @app.teardown_appcontext
 def teardown(self):
-    """
-    closes session
-    """
+    """Close session"""
     storage.close()
 
 
 @app.route('/states_list')
-def p_states():
-    """renders states"""
+def states():
+    """Display all states"""
     states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
+    return render_template('7-states_list.html', states=states)
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
