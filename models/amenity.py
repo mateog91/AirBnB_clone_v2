adfd0702a1d746282cc7397e6e43586c4d053cfc
Mateo <mateog91@gmail.com>
2022-01-31 19:43:39 -0500

    change in amenity, checking with cristhian

--- models/amenity.py
+++ models/amenity.py
@@ -11,5 +11,4 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
     place_amenities = relationship(
-        'Place', secondary='place_amenity', viewonly=False,
-        overlaps='place_amenities')
+        'Place', secondary='place_amenity')
