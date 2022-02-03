733b198e17f68bdf4d44bd1a7557b56fbd09fccd
Mateo <mateog91@gmail.com>
2022-02-02 19:52:02 -0500

    FIX: for loop and correct url

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,12 +10,12 @@ from models.city import City
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def p_states():
     states = storage.all(State).values()
     cities = storage.all(City).values()
     """renders states"""
-    return render_template("7-states_list.html", states=states, cities=cities)
+    return render_template("8-cities_by_states.html", states=states, cities=cities)
 
 
 @app.teardown_appcontext
@@ -25,4 +25,4 @@ def teardown(self):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
